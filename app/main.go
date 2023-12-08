bef74a41d938d59798b358fd715040ec500a00b9
mokori4242 <mokori4242@gmail.com>
2023-12-08 15:21:00 +0900

    [add]6.User:サーバー立ち上げるためにmain関数を実装

---
+++ app/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"app/controller"
+	"app/db"
+	"app/repository"
+	"app/router"
+	"app/usecase"
+)
+
+func main() {
+	db := db.NewDB()
+	userRepository := repository.NewUserRepository(db)
+	userUsecase := usecase.NewUserUsecase(userRepository)
+	userController := controller.NewUserController(userUsecase)
+	e := router.NewRouter(userController)
+	e.Logger.Fatal(e.Start(":8080"))
+}
